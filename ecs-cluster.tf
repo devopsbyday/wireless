728a6f4e59ae14a0a1b237bf8b831d8bfb5e80e6
Mark McLeod <mark.mcleod@tecpeopleservices.com>
2024-06-17 13:08:37 +0100

    Ticket #1 changes to elasticache module and project structure

--- ecs-cluster.tf
+++ ecs-cluster.tf
@@ -1,6 +1,33 @@
-module "ec2-scheduler"{
-    source = "./elasticache"
+module "elasticache" {
+  source = "terraform-aws-modules/elasticache/aws"
 
-    count = local.create ? 1 : 0
+  cluster_id               = "tps-wl-redis"
+  create_cluster           = true
+  create_replication_group = false
 
+  engine_version = "7.1"
+  node_type      = "cache.t4g.micro"
+
+  vpc_id = module.vpc.vpc_id
+  security_group_rules = {
+    ingress_vpc = {
+      description = "TPS Wireless Cache Ingress"
+      cidr_ipv4   = module.vpc.vpc_cidr_block
+    }
+  }
+
+  subnet_ids = module.vpc.private_subnets
+
+  create_parameter_group = true
+  parameter_group_family = "redis7-and-above"
+  parameters = [
+    {
+      name  = "latency-tracking"
+      value = "yes"
+    }
+  ]
+
+  tags = {
+    Environment = "tps-dev"
+  }
 }
